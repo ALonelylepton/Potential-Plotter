dba19bfe42fef20963f3fee4078e85237de5e9aa
ALonelylepton <billsmith34124@gmail.com>
2023-07-22 21:00:44 +0100

    mods

--- functions.py
+++ functions.py
@@ -1,9 +1,9 @@
 import numpy as np
 
 
-def potentialShape(chi):
+def potentialShape(pos):
     chi_zero=1
-    potential=(chi**2-chi_zero**2)**2
+    potential=(pos[0]**2-chi_zero**2)**2+pos[1]**2 + pos[1]**2*pos[0]**2
     return(potential)
 
 def localPotential(chi_pos, chi_vel, phi_pos, phi_vel, dt):
@@ -11,22 +11,23 @@ def localPotential(chi_pos, chi_vel, phi_pos, phi_vel, dt):
     local_chi=np.array([chi_pos-dt/2,chi_pos,chi_pos+dt/2])
     local_phi=np.array([phi_pos-dt/2,phi_pos,phi_pos+dt/2])
     
-    chi_mesh, phi_mesh = np.meshgrid(local_chi, local_phi)
+    phi_mesh, chi_mesh = np.meshgrid(local_phi, local_chi)
     
     potential=(chi_mesh**2-chi_zero**2)**2 + phi_mesh**2 +phi_mesh**2*chi_mesh**2
     return(potential)
 
-def getAcc(chi_pos, chi_vel, phi_pos, phi_vel, dt):
+def getAcc(pos, vel, dt):
     #get local potential shape
-    potential=localPotential(chi_pos, chi_vel, phi_pos, phi_vel, dt)
+    potential=localPotential(pos[0], vel[0], pos[1], vel[1], dt)
     #get hubble parameter
-    H=np.sqrt((potential+chi_vel**2/2)/3)
-    #get acceleration
+    H=np.sqrt((potential+vel[0]**2/2)/3)
+    #get acceleration 0 is chi, 1 is phi
     acc=np.gradient(potential)
-    phi_acc=acc[0][1][1]*-1
-    chi_acc=acc[1][1][1]*-1
+    acc[0]=acc[0]*-1
+    acc[1]=acc[1]*-1
+    acc_local=np.array([acc[0][1][1], acc[1][1][1]])
     '''
     need to add -3*H*vel
     '''
     
-    return(chi_acc, phi_acc)
+    return(acc_local)
--- main.py
+++ main.py
@@ -9,47 +9,51 @@ dt= 0.1    #time step
 plotRealTime=True
 #total simulation length
 tot=int(Nt/dt)
-#initial conditions
-chi_pos, chi_vel=2, 0.01
-phi_pos, phi_vel=2,0.01
+#initial conditions (0 is chi, 1 is phi)
+pos=np.array([2.,3.])
+vel=np.array([0.01,0.01])
 #trajectory records
 pos_save=np.zeros((tot,2))  #chi position record
-pos_save[0][0], pos_save[0][1]=chi_pos, phi_pos
-pot_save=np.zeros((tot,2))  #potential record
-pot_save[0]=func.potentialShape(chi_pos)
-#calculate initial acceleration
-chi_acc, phi_acc=func.getAcc(chi_pos,chi_vel, phi_pos, phi_vel, dt)
+pos_save[0] =pos
+pot_save=np.zeros((tot,1))  #potential record
+pot_save[0]=func.potentialShape(pos)
+#calculate initial acceleration (0 is chi, 1 is phi)
+acc=func.getAcc(pos,vel, dt)
 #%%
 #set up figure
 fig=plt.figure(dpi=80)
 grid=plt.GridSpec(3,1,wspace=0.0,hspace=0.03)
-ax=plt.subplot(grid[:,0])
+ax=plt.subplot(grid[:,0], projection='3d')
 for i in range(tot):
+    '''update fields'''
     #kick
-    chi_vel+=chi_acc*dt/2
+    vel+=acc*dt/2
     #update
-    chi_pos+=chi_vel*dt
-    pos_save[i]=chi_pos
-    pot_save[i]=func.potentialShape(chi_pos)
+    pos+=vel*dt
+    pos_save[i]=pos
+    pot_save[i]=func.potentialShape(pos)
     #get acc
-    chi_acc=func.getAcc(chi_pos, chi_vel, dt)
+    acc=func.getAcc(pos, vel, dt)
     #kick
-    chi_vel+=chi_acc*dt/2
+    vel+=acc*dt/2
+    
     #run plot updater
     if plotRealTime or (i==tot-1):
         plt.sca(ax)
         plt.cla()
-        x=pos_save[i]
-        y=pot_save[i]
-        plt.scatter(x,y)
-        ax.set(xlim=(-5,5), ylim=(0,10))
-        ax.set_aspect('equal','box')
+        x=pos_save[i][0]
+        y = pos_save[i][1]
+        z=pot_save[i]
+        print(z)
+        ax.scatter(x,y,z)
+        ax.set(xlim=(-5,5), ylim=(-5,5), zlim=(0, 50))
+        
         #print maxima
     conditions=np.array([pot_save[i-1]>pot_save[i-2], pot_save[i-1]>pot_save[i], pot_save[i-1]>3])
     if conditions.all():
         print(pot_save[i], pot_save[i-1],pot_save[i+1])
         print('-----------------------------------------------------')
-    plt.pause(0.01)
+    plt.pause(0.1)
     
     
     #print(chi_vel)
